f6721d4246f1901a0ceb9f2298d90dde27bbf4fa
crborga <chrisborga@yahoo.com>
2024-04-05 01:01:34 -0400

    Add files via upload
    Updated DBM to ChromieCraft fork. Fixed Questhelper installer.

--- AddOns.ps1
+++ AddOns.ps1
@@ -1,4 +1,4 @@
-$global:addons = "F:\Wow\Wrath of the Lich King\Interface\AddOns"
+$global:addons = "E:\WOW_WOTLK\Interface\Addons1"
 $global:github = "https://www.github.com"
 function Show-Menu
 {
@@ -8,18 +8,22 @@ function Show-Menu
 	Clear-Host	
 	Write-Host "============== $Title ================"
 	Write-Host ""
-	Write-Host "1: AtlasLoot"
-	Write-Host "2: DBM"
-	Write-Host "3: ElvUI"
-	Write-Host "4: Skada"
-	Write-Host "5: AuctionHouseDepositFixer"
+	Write-Host "          1: AtlasLoot"
+	Write-Host "          2: DBM"
+	Write-Host "          3: ElvUI"
+	Write-Host "          4: ElvUI AddOnSkins"
+	Write-Host "          5: ElvUI Enhanced"
+	Write-Host "          6: QuestHelper"
+	Write-Host "          7: Skada"
+	Write-Host "          8: AuctionHouseDepositFixer"
     Write-Host ""
-	Write-Host "Q: Press 'Q' to quit."
-	Write-Host "======================================"
+	Write-Host "          L: Show AddOns"
+    Write-Host ""
+	Write-Host "========================================================="
 	}
 
 Show-Menu -Title 'ChromieCraft AddOn Helper'
- $selection = Read-Host "Please make a selection"
+ $selection = Read-Host "Please make a selection or Q to exit"
  switch ($selection)
  {
      '1' {
@@ -33,11 +37,11 @@ Show-Menu -Title 'ChromieCraft AddOn Helper'
 		 } 
 	 '2' {  
 		  Write-Host '================= Installing DBM ========================'
-		  Invoke-Webrequest "$github/manacute/DBM-Warmane/archive/refs/heads/main.zip" -Out "$addons\dbm.zip"
-		  $dbm = "$addons\DBM-Warmane-main\DBM-*"
+		  Invoke-Webrequest "$github/balleny/DBM/archive/refs/heads/main.zip" -Out "$addons\dbm.zip"
+		  $dbm = "$addons\DBM-main\DBM-*"
 		  Expand-Archive "$addons\dbm.zip" -DestinationPath "$addons"
 		  Copy-Item "$dbm" "$addons" -Recurse -Force
-		  Remove-Item "$addons\DBM-Warmane-main" -Recurse -Force
+		  Remove-Item "$addons\DBM-main" -Recurse -Force
 		  Remove-Item "$addons\dbm.zip"
 		 } 
   	 '3' {
@@ -48,8 +52,35 @@ Show-Menu -Title 'ChromieCraft AddOn Helper'
 		  Copy-Item "$elvui" "$addons" -Recurse -Force
 		  Remove-Item "$addons\elvui.zip"
 		  Remove-Item "$addons\ElvUI-master" -Recurse -Force
-		 } 
-	 '4' {
+		 }
+  	 '4' {
+          Write-Host '============ Installing ElvUI AddonSkins ================'
+		  Invoke-Webrequest "$github/ElvUI-WotLK/ElvUI_AddOnSkins/archive/refs/heads/master.zip" -Out "$addons\elvui.zip"
+		  $elvui = "$addons\ElvUI_AddOnSkins-master\ElvUI*"
+		  Expand-Archive "$addons\elvui.zip" -DestinationPath "$addons"
+		  Copy-Item "$elvui" "$addons" -Recurse -Force
+		  Remove-Item "$addons\elvui.zip"
+		  Remove-Item "$addons\ElvUI_AddOnSkins-master" -Recurse -Force
+		 }
+  	 '5' {
+          Write-Host '============ Installing ElvUI Enhanced ================'
+		  Invoke-Webrequest "$github/ElvUI-WotLK/ElvUI_Enhanced/archive/refs/heads/master.zip" -Out "$addons\elvui.zip"
+		  $elvui = "$addons\ElvUI_Enhanced-master\ElvUI*"
+		  Expand-Archive "$addons\elvui.zip" -DestinationPath "$addons"
+		  Copy-Item "$elvui" "$addons" -Recurse -Force
+		  Remove-Item "$addons\elvui.zip"
+		  Remove-Item "$addons\ElvUI_Enhanced-master" -Recurse -Force
+		 }	
+	 '6' {
+		  Write-Host '=================== Installing QuestHelper ===================='
+		  Invoke-Webrequest "$github/zorbathut/questhelper/archive/refs/heads/master.zip" -Out "$addons\qh.zip"
+		  $qh = "$addons\questhelper-master\*"
+		  Expand-Archive "$addons\qh.zip" -DestinationPath "$addons"
+		  Copy-Item "$qh" "$addons\QuestHelper" -Recurse -Force
+		  Remove-Item "$addons\qh.zip"
+		  Remove-Item "$addons\questhelper-master" -Recurse -Force  
+		 }
+	 '7' {
 		  Write-Host '=================== Installing Skada ===================='
 		  Invoke-Webrequest "$github/bkader/Skada-WoTLK/archive/refs/heads/main.zip" -Out "$addons\skada.zip"
 		  $skada = "$addons\Skada-WoTLK-main\Skada*"
@@ -58,7 +89,7 @@ Show-Menu -Title 'ChromieCraft AddOn Helper'
 		  Remove-Item "$addons\skada.zip"
 		  Remove-Item "$addons\Skada-WoTLK-main" -Recurse -Force
 		 }
-     '5' {
+     '8' {
 		  Write-Host '========= Installing AuctionHouseDepositFixer ==========='
 		  Invoke-Webrequest "$github/Wrath-AddOns/AuctionHouseDepositFixer/archive/refs/heads/master.zip" -Out "$addons\ahdepfix.zip"
 		  $ahdepfix = "$addons\AuctionHouseDepositFixer-master\AuctionHouseDepositFixer"
@@ -66,7 +97,11 @@ Show-Menu -Title 'ChromieCraft AddOn Helper'
 		  Copy-Item "$ahdepfix" "$addons" -Recurse -Force
 		  Remove-Item "$addons\AuctionHouseDepositFixer-master" -Recurse -Force
 		  Remove-Item "$addons\ahdepfix.zip"
-		 }	
+		 }
+	'l' {
+	 Get-ChildItem $addons | Format-Wide -Column 5
+	 pause
+	}
 	'q'  {
          return
 		 }
@@ -75,7 +110,7 @@ Show-Menu -Title 'ChromieCraft AddOn Helper'
 do
  {
      Show-Menu
-     $selection = Read-Host "Please make a selection"
+     $selection = Read-Host "Please make a selection or Q to exit"
      switch ($selection)
      {
      '1' {
@@ -89,11 +124,11 @@ do
 		 } 
 	 '2' {  
 		  Write-Host '================= Installing DBM ========================'
-		  Invoke-Webrequest "$github/manacute/DBM-Warmane/archive/refs/heads/main.zip" -Out "$addons\dbm.zip"
-		  $dbm = "$addons\DBM-Warmane-main\DBM-*"
+		  Invoke-Webrequest "$github/balleny/DBM/archive/refs/heads/main.zip" -Out "$addons\dbm.zip"
+		  $dbm = "$addons\DBM-main\DBM-*"
 		  Expand-Archive "$addons\dbm.zip" -DestinationPath "$addons"
 		  Copy-Item "$dbm" "$addons" -Recurse -Force
-		  Remove-Item "$addons\DBM-Warmane-main" -Recurse -Force
+		  Remove-Item "$addons\DBM-main" -Recurse -Force
 		  Remove-Item "$addons\dbm.zip"
 		 } 
   	 '3' {
@@ -105,7 +140,34 @@ do
 		  Remove-Item "$addons\elvui.zip"
 		  Remove-Item "$addons\ElvUI-master" -Recurse -Force
 		 } 
-	 '4' {
+  	 '4' {
+          Write-Host '============ Installing ElvUI AddonSkins ================'
+		  Invoke-Webrequest "$github/ElvUI-WotLK/ElvUI_AddOnSkins/archive/refs/heads/master.zip" -Out "$addons\elvui.zip"
+		  $elvui = "$addons\ElvUI_AddOnSkins-master\ElvUI*"
+		  Expand-Archive "$addons\elvui.zip" -DestinationPath "$addons"
+		  Copy-Item "$elvui" "$addons" -Recurse -Force
+		  Remove-Item "$addons\elvui.zip"
+		  Remove-Item "$addons\ElvUI_AddOnSkins-master" -Recurse -Force
+		 }
+  	 '5' {
+          Write-Host '============ Installing ElvUI Enhanced ================'
+		  Invoke-Webrequest "$github/ElvUI-WotLK/ElvUI_Enhanced/archive/refs/heads/master.zip" -Out "$addons\elvui.zip"
+		  $elvui = "$addons\ElvUI_Enhanced-master\ElvUI*"
+		  Expand-Archive "$addons\elvui.zip" -DestinationPath "$addons"
+		  Copy-Item "$elvui" "$addons" -Recurse -Force
+		  Remove-Item "$addons\elvui.zip"
+		  Remove-Item "$addons\ElvUI_Enhanced-master" -Recurse -Force
+		 }		 
+	 '6' {
+		  Write-Host '=================== Installing QuestHelper ===================='
+		  Invoke-Webrequest "$github/zorbathut/questhelper/archive/refs/heads/master.zip" -Out "$addons\qh.zip"
+		  $qh = "$addons\questhelper-master\*"
+		  Expand-Archive "$addons\qh.zip" -DestinationPath "$addons"
+		  Copy-Item "$qh" "$addons\QuestHelper" -Recurse -Force
+		  Remove-Item "$addons\qh.zip"
+		  Remove-Item "$addons\questhelper-master" -Recurse -Force  
+		 }
+	 '7' {
 		  Write-Host '=================== Installing Skada ===================='
 		  Invoke-Webrequest "$github/bkader/Skada-WoTLK/archive/refs/heads/main.zip" -Out "$addons\skada.zip"
 		  $skada = "$addons\Skada-WoTLK-main\Skada*"
@@ -114,7 +176,7 @@ do
 		  Remove-Item "$addons\skada.zip"
 		  Remove-Item "$addons\Skada-WoTLK-main" -Recurse -Force
 		 }
-     '5' {
+     '8' {
 		  Write-Host '========= Installing AuctionHouseDepositFixer ==========='
 		  Invoke-Webrequest "$github/Wrath-AddOns/AuctionHouseDepositFixer/archive/refs/heads/master.zip" -Out "$addons\ahdepfix.zip"
 		  $ahdepfix = "$addons\AuctionHouseDepositFixer-master\AuctionHouseDepositFixer"
@@ -122,7 +184,11 @@ do
 		  Copy-Item "$ahdepfix" "$addons" -Recurse -Force
 		  Remove-Item "$addons\AuctionHouseDepositFixer-master" -Recurse -Force
 		  Remove-Item "$addons\ahdepfix.zip"
-		 }   	
+		 }
+	'l' {
+	Get-ChildItem $addons | Format-Wide -Column 5	
+	pause
+	}		 
 	'q'  {
          return
 		 }
